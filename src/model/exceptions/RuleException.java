b97a0b5c9686075a51ad623d768e132bd894aa34
haytrun <haytrun@a8733c2a-9e95-11de-a36b-d11e950a0b04>
2009-10-21 13:05:42 +0000

    final update

--- src/model/exceptions/RuleException.java
+++
@@ -1,8 +0,0 @@
-package model.exceptions;
-
-/** Exception in graph transformation */
-public class RuleException extends Exception {
-	public RuleException(String string) {
-		super(string);
-	}
-}
