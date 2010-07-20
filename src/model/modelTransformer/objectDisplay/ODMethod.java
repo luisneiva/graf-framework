d7d8012a1e0d2eba33cd448e5ef59fb11049761e
oscarw44 <oscarw44@a8733c2a-9e95-11de-a36b-d11e950a0b04>
2010-07-20 09:53:04 +0000

    Added ODMethod class which doesn't do anything yet

---
+++ src/model/modelTransformer/objectDisplay/ODMethod.java
@@ -0,0 +1,14 @@
+package model.modelTransformer.objectDisplay;
+
+public class ODMethod {
+	
+	private String name;
+
+	public ODMethod(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
