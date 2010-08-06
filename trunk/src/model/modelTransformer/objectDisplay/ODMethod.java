23f4f9332c4c70092c3c480b361feae9d5e3f7c2
a.farquharson07 <a.farquharson07@a8733c2a-9e95-11de-a36b-d11e950a0b04>
2010-08-06 10:06:55 +0000

    Added logic to determine whether a method is 'enabled'

--- trunk/src/model/modelTransformer/objectDisplay/ODMethod.java
+++ trunk/src/model/modelTransformer/objectDisplay/ODMethod.java
@@ -1,6 +1,9 @@
 package model.modelTransformer.objectDisplay;
 
+import java.util.ArrayList;
+
 import model.ListGraph;
+import agg.xt_basis.Arc;
 import agg.xt_basis.Node;
 
 //import agg.xt_basis.Node;
@@ -18,11 +21,37 @@ public class ODMethod {
 	public ODMethod(Node node) {
 		name = ListGraph.getName(node);
 		
-		//TODO - change constructor to accept a node - the node that instantiates the method
-		// Then, assign value to type by tracing i-edge.
+		type = "MethodCall";
+
+//		for(Arc fromEdge : node.getOutgoingArcsVec()) {
+//			if(ListGraph.getName(fromEdge).equals("i")) {
+//				type = ListGraph.getName(fromEdge.getTarget());
+//				break;
+//			}
+//		}
 		
-		//TODO - assign value to enabled by checking whether all the 'entry' actions have
-		// been executed?
+		// The method will be available for execution when:
+		// 1) it has no behaviorExecution node (it has not been executed before)
+		// 2) it has a bE node, but that node has no executable edges (it has been
+		//    executed before, but the execution is complete)
+		ArrayList<String> traceToBE = new ArrayList<String>();
+		traceToBE.add("behavior");
+		
+		ArrayList<Node> possibleBeNodes = ListGraph.fromTrace(traceToBE, node);
+		
+		ArrayList<String> traceToBeEdges = new ArrayList<String>();
+		traceToBeEdges.add("executable");
+		
+		if (possibleBeNodes.isEmpty()) {
+//			System.out.println("NO BE Node found for " + this.getName());
+			enabled = true ;
+		} else if (ListGraph.toTrace(traceToBeEdges, possibleBeNodes.get(0)).isEmpty()) {
+//			System.out.println("BE Node found for " + this.getName() + " but it has no ExEdges!");
+			enabled = true ;
+		} else {
+//			System.out.println("BE Node found for " + this.getName() + " and it has an ExEdge!");
+			enabled = false ;
+		}
 	}
 
 	/**
