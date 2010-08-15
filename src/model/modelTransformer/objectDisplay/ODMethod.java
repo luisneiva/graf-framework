d8f01c06bbd954d4415e277d07d162e41cb8f482
a.farquharson07 <a.farquharson07@a8733c2a-9e95-11de-a36b-d11e950a0b04>
2010-08-15 06:17:33 +0000

    Changes to allow methods to be 'active' or not

--- src/model/modelTransformer/objectDisplay/ODMethod.java
+++ src/model/modelTransformer/objectDisplay/ODMethod.java
@@ -16,19 +16,11 @@ public class ODMethod {
 
 	/**
 	 * @param node The node that instantiates the method
-	 * @param name The name of this node
 	 */
 	public ODMethod(Node node) {
 		name = ListGraph.getName(node);
 		
 		type = "MethodCall";
-
-//		for(Arc fromEdge : node.getOutgoingArcsVec()) {
-//			if(ListGraph.getName(fromEdge).equals("i")) {
-//				type = ListGraph.getName(fromEdge.getTarget());
-//				break;
-//			}
-//		}
 		
 		// The method will be available for execution when:
 		// 1) it has no behaviorExecution node (it has not been executed before)
@@ -43,13 +35,10 @@ public class ODMethod {
 		traceToBeEdges.add("executable");
 		
 		if (possibleBeNodes.isEmpty()) {
-//			System.out.println("NO BE Node found for " + this.getName());
 			enabled = true ;
 		} else if (ListGraph.toTrace(traceToBeEdges, possibleBeNodes.get(0)).isEmpty()) {
-//			System.out.println("BE Node found for " + this.getName() + " but it has no ExEdges!");
 			enabled = true ;
 		} else {
-//			System.out.println("BE Node found for " + this.getName() + " and it has an ExEdge!");
 			enabled = false ;
 		}
 	}
