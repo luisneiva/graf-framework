770ae1d7c82740e953fbbbd30818009d33ea41ff
a.farquharson07 <a.farquharson07@a8733c2a-9e95-11de-a36b-d11e950a0b04>
2010-08-28 07:42:56 +0000

    Made methods always clickable.

--- src/model/modelTransformer/objectDisplay/ODMethod.java
+++ src/model/modelTransformer/objectDisplay/ODMethod.java
@@ -22,25 +22,7 @@ public class ODMethod {
 		
 		type = "CallOperationAction";
 		
-		// The method will be available for execution when:
-		// 1) it has no behaviorExecution node (it has not been executed before)
-		// 2) it has a bE node, but that node has no executable edges (it has been
-		//    executed before, but the execution is complete)
-		ArrayList<String> traceToBE = new ArrayList<String>();
-		traceToBE.add("behavior");
-		
-		ArrayList<Node> possibleBeNodes = ListGraph.fromTrace(traceToBE, node);
-		
-		ArrayList<String> traceToBeEdges = new ArrayList<String>();
-		traceToBeEdges.add("executable");
-		
-		if (possibleBeNodes.isEmpty()) {
-			enabled = true ;
-		} else if (ListGraph.toTrace(traceToBeEdges, possibleBeNodes.get(0)).isEmpty()) {
-			enabled = true ;
-		} else {
-			enabled = false ;
-		}
+		enabled = true ;
 	}
 
 	/**
