fb70dd7ff358bd7b5817d11748fbf2105d7200f9
poisonthemon <poisonthemon@a8733c2a-9e95-11de-a36b-d11e950a0b04>
2010-08-31 05:47:36 +0000

    Further modified the properties class to allow for a new properties file to be read in -- Stephen

--- trunk/src/controller/Properties.java
+++ trunk/src/controller/Properties.java
@@ -10,16 +10,15 @@ import java.util.Hashtable;
 import java.util.Scanner;
 
 /**
- * A class that contains 3 static Boolean flags to turn on/off debug/status messages in the console.
+ * A class that contains a table of properties
  * @author Frank Su
+ * @author Stephen Gream
  *
  */
 public class Properties
 {
-	//@Deprecated
-	//public static Boolean printRules, printGraphs, printDebug;
 	private final static Hashtable<String,String> properties = new Hashtable<String,String>();
-	
+	private static String propFile = "Properties.txt";
 	
 	
 	// menu: Properties -> print...
@@ -27,7 +26,12 @@ public class Properties
 	 * Read in a properties file
 	 */
 	public synchronized static void readProperties() throws FileNotFoundException {
-		Scanner sc = new Scanner(new File("Properties.txt"));
+		Enumeration<String> keys = properties.keys();
+		while(keys.hasMoreElements()){
+			String key = keys.nextElement();
+			properties.remove(key);
+		}
+		Scanner sc = new Scanner(new File(propFile));
 		String lineStr = "";
 		String content[];
 		try {
@@ -35,15 +39,34 @@ public class Properties
 			while(sc.hasNextLine()) {
 				lineStr = sc.nextLine();
 				content = lineStr.split(":");
-
-//				if(lineStr.startsWith("PrintRules")) {
-//					printRules = Boolean.parseBoolean(content[1]);
-//				} else if(lineStr.startsWith("PrintGraphs")) {
-//					printGraphs = Boolean.parseBoolean(content[1]);
-//				} else if(lineStr.startsWith("PrintDebug")) {
-//					printDebug = Boolean.parseBoolean(content[1]);
-//				}
-
+				properties.put(content[0], content[1]);
+			}
+		}
+		finally {
+			//ensure the underlying stream is always closed
+			sc.close();
+		}
+	}
+	
+	/**
+	 * Read in a properties file
+	 * @param filePath Path to the properties file to be read
+	 */
+	public synchronized static void readProperties(String filePath) throws FileNotFoundException {
+		Enumeration<String> keys = properties.keys();
+		while(keys.hasMoreElements()){
+			String key = keys.nextElement();
+			properties.remove(key);
+		}
+		propFile = filePath;
+		Scanner sc = new Scanner(new File(filePath));
+		String lineStr = "";
+		String content[];
+		try {
+			//first use a Scanner to get each line
+			while(sc.hasNextLine()) {
+				lineStr = sc.nextLine();
+				content = lineStr.split(":");
 				properties.put(content[0], content[1]);
 			}
 		}
@@ -63,7 +86,7 @@ public class Properties
 		String towrite = "";
 		
 		try {
-			fstream = new FileWriter("Properties.txt");
+			fstream = new FileWriter(propFile);
 			BufferedWriter out = new BufferedWriter(fstream);
 			//towrite = "PrintRules:" + printRules + "\nPrintGraphs:" + printGraphs + "\nPrintDebug:" + printDebug; 
 			Enumeration<String> keys = properties.keys();
