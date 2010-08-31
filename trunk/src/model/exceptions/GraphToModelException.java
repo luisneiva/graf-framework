39f10267d17df43ee167e67e3cf66cdbcdf9d019
poisonthemon <poisonthemon@a8733c2a-9e95-11de-a36b-d11e950a0b04>
2010-08-31 05:21:29 +0000

    Added standalone package (graf.tar.bz2) and deleted bin directory -- Stephen

--- trunk/src/model/exceptions/GraphToModelException.java
+++
@@ -1,8 +0,0 @@
-package model.exceptions;
-
-/** Exception for transformation of graph to model */
-public class GraphToModelException extends Exception {
-	public GraphToModelException(String string) {
-		super(string);
-	}
-}
