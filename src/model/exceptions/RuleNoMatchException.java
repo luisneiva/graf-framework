ede56d43c5855723a7c30b1852a77180563aaf76
oscarw44 <oscarw44@a8733c2a-9e95-11de-a36b-d11e950a0b04>
2010-09-23 07:39:47 +0000

    Deleted bin

--- src/model/exceptions/RuleNoMatchException.java
+++
@@ -1,8 +0,0 @@
-package model.exceptions;
-
-/** Rule did not match when trying to apply transformation */
-public class RuleNoMatchException extends RuleException {
-	public RuleNoMatchException(String string) {
-		super(string);
-	}
-}
