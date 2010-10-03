0c509599f7a75b33c38ce350b816ea1916a2a8db
franksdp32 <franksdp32@a8733c2a-9e95-11de-a36b-d11e950a0b04>
2010-10-03 10:03:34 +0000

    Rename GrafTimer.java (deleted Timer.java)

--- trunk/src/controller/GrafTimer.java
+++ trunk/src/controller/GrafTimer.java
@@ -1,13 +1,17 @@
 package controller;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
+
+import javax.swing.Timer;
 
 import agg.xt_basis.Arc;
 import agg.xt_basis.Node;
 import model.ListGraph;
 import model.modelTransformer.objectDisplay.ODObject;
 
-public class Timer {
+public class GrafTimer {
 
 	/** 
 	 * current time must be static because a new timer object
@@ -25,12 +29,18 @@ public class Timer {
 	 */
 	private ArrayList<ODObject> timedObjects;
 	
+	private Integer timerValue = 0;
+	
+	private Timer t;
+	private Integer delaySec;  // milliseconds between timer ticks
+	
 	//TODO add a listener attribute
 	
-	public Timer(ListGraph graph, ArrayList<ODObject> objects) {
+	public GrafTimer(ListGraph graph, ArrayList<ODObject> objects) {
 		//TODO set timedObjects:
 		// timedObjects is the objects which have sent a message to the actor
 		
+		timedObjects = new ArrayList<ODObject>();
 		// First find the Actor node
 		Node actor = null;
 		for(Arc edge : graph.getArcsList()) {
@@ -53,18 +63,55 @@ public class Timer {
 		findObjectRoute.add("pool");
 		findObjectRoute.add("message");
 		findObjectRoute.add("sender");
+		
+		ArrayList<String> findSetTimerRoute = new ArrayList<String>();
+		findSetTimerRoute.add("pool");
+		findSetTimerRoute.add("message");
+		findSetTimerRoute.add("i");
+		findSetTimerRoute.add("ownedAttribute");
+		findSetTimerRoute.add("defaultValue");
 
 		// Find objects
-		ArrayList<Node> findObjects = new ArrayList<Node>(); 
+		ArrayList<Node> findObjects = new ArrayList<Node>();
+		// Find Timer property: setTimer
+		ArrayList<Node> findTimerProperty = new ArrayList<Node>();
+		
 		for(Node actorInstance : actorInstances) {
 			findObjects = ListGraph.toTrace(findObjectRoute, actorInstance);
 			System.out.println("Timer: Find an object -> size: " + findObjects.size());
 			for (Node objectNode : findObjects) {
 				System.out.println("Find an object -> name: " + ListGraph.getName(objectNode));
+				//add object to timedObjects. add to timedObjects the ODObject in the input parameter "objects" that has the same name m1.
+				for(ODObject odo : objects) {
+					System.out.println("odo's name: " + odo.getName());
+					if(odo.getName().equals(ListGraph.getName(objectNode))) {
+						timedObjects.add(odo);
+					}
+				}
+			}
+			
+			findTimerProperty = ListGraph.toTrace(findSetTimerRoute, actorInstance);
+			for (Node timerPropNode : findTimerProperty) {
+				timerValue = Integer.parseInt(ListGraph.getName(timerPropNode));
+				System.out.println("Find Timer property -> timer value: " + timerValue);
+				delaySec = timerValue * 1000;
+				
+				class AntMovementTimerListener implements ActionListener
+				{
+					int randX = 0, randY = 0;
+					Double d;
+					public void actionPerformed(ActionEvent event) {
+						
+					}
+				}
+
+				ActionListener antMoveListener = new AntMovementTimerListener();
+				t = new Timer(delaySec, antMoveListener);
+				t.start();
 			}
 		}
 		
-		
+		System.out.println("\tFinding object OVER. Look for Timer property");
 		
 		//TODO set time by looking at the property of the received signal
 	}
