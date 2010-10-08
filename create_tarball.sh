8c713b1cba15b2d98dce335a72da2ab08c7a8d5d
poisonthemon <poisonthemon@a8733c2a-9e95-11de-a36b-d11e950a0b04>
2010-10-08 01:06:22 +0000

    Added script to create distributable tarball -- Stephen

---
+++ create_tarball.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Creating GRAF tarball...."
+tar -cvvf graf.tar graf.jar GTS* UMLMetamodel Properties.txt Models > /dev/null
+bzip2 graf.tar > /dev/null
+echo "Done creating graf.tar.bz2"
