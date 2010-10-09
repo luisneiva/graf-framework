9f523da7af410b161dc606934684c7652c32ca59
poisonthemon <poisonthemon@a8733c2a-9e95-11de-a36b-d11e950a0b04>
2010-10-09 01:00:00 +0000

    Fixed create_tarball.sh

--- create_tarball.sh
+++ create_tarball.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 echo "Creating GRAF tarball...."
-tar -cvvf graf.tar ./dist/graf.jar GTS* UMLMetamodel Properties.txt Models > /dev/null
+cp ./dist/graf.jar ./ -v
+tar -cvvf graf.tar ./graf.jar GTS* UMLMetamodel Properties.txt Models GraphOutputs > /dev/null
 bzip2 graf.tar > /dev/null
+rm ./graf.jar
 echo "Done creating graf.tar.bz2"
